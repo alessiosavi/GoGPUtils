7a512474cf79ab98f8daf2d0c52668b4f9c1cdc3
alessiosavi <alessiosavibtc@gmail.com>
2019-10-25 09:18:51 +0200

    Add method for work with http - SetHeaders: the method is delegated to set the given headers data (key-value list) into the (request) response; - ServeHeaders: the method is delegated to spawn a webserver for set the input headers - Create test for cookie/headers method
    BugFix: ServeCookie and DebugRequest will now close the server once done (defer)

--- http/httputils.go
+++ http/httputils.go
@@ -41,6 +41,62 @@ func CreateCookie(name, value, domain, path string, maxage int, httpOnly bool) (
 	return &cookie, nil
 }
 
+func SetHeaders(headersList []string, w http.ResponseWriter) error {
+	if headersList == nil {
+		return errors.New("headers list not provided")
+	} else if len(headersList)%2 != 0 {
+		return errors.New("headers are not a key-value list")
+	}
+	for i := 0; i < len(headersList); i += 2 {
+		w.Header().Set(headersList[i], headersList[i+1])
+	}
+	return nil
+}
+
+// ServeHeaders is delegated to spawn a webserver for set  call
+func ServeHeaders(headersList []string, ip, port, endpoint string) error {
+	// Validate input
+	if stringutils.IsBlank(ip) {
+		return errors.New("hostname/ip not provided")
+	}
+	if stringutils.IsBlank(port) {
+		return errors.New("port not provided")
+	}
+	if stringutils.IsBlank(endpoint) {
+		endpoint = "/"
+	}
+
+	// Instantiate server
+	m := http.NewServeMux()
+	s := http.Server{Addr: ip + ":" + port, Handler: m}
+	defer s.Close()
+
+	if headersList == nil {
+		headersList = []string{"Access-Control-Allow-Origin", "origin", "Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE", "Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"}
+	}
+
+	// Bind the endpoint for instantiate the cookie
+	m.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		SetHeaders(headersList, w)
+		w.WriteHeader(200)
+		go http.Get(`http://` + ip + `:` + port + `/shutdown`)
+	})
+
+	// Bind the endpoint for shutdown the server
+	m.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		s.Shutdown(context.Background())
+	})
+
+	// Serve the http webserver
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
+
 // ServeCookie is delegated to set a cookie and shutdown the server after the first call
 func ServeCookie(ip, port, endpoint, name, value, domain, path string, maxage int, httpOnly bool) error {
 
@@ -58,7 +114,7 @@ func ServeCookie(ip, port, endpoint, name, value, domain, path string, maxage in
 	// Instantiate server
 	m := http.NewServeMux()
 	s := http.Server{Addr: ip + ":" + port, Handler: m}
-
+	defer s.Close()
 	// Create cookie
 	cookie, err := CreateCookie(name, value, domain, path, maxage, httpOnly)
 	if err != nil {
@@ -103,6 +159,7 @@ func DebugRequest(ip, port, endpoint string) error {
 	// Instantiate server
 	m := http.NewServeMux()
 	s := http.Server{Addr: ip + ":" + port, Handler: m}
+	defer s.Close()
 	// Bind the endpoint for instantiate the cookie
 	m.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
 		data, err := httputil.DumpRequest(r, true)
