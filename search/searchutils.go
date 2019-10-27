a59c1fee6a4627a326708d894d72084782d0683a
alessiosavi <Alessio.Savi@ibm.com>
2019-10-27 13:54:05 +0100

    - Fix documentation

--- search/searchutils.go
+++ search/searchutils.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 )
 
+// LinearSearchParallelInt is delegated to parallelize the execution of search method
 func LinearSearchParallelInt(data []int, target int, thread int) int {
 	length := len(data)
 	dataXThread := length / thread
@@ -14,9 +15,8 @@ func LinearSearchParallelInt(data []int, target int, thread int) int {
 		found := LinearSearchInt(data[thread*dataXThread:oddment], target)
 		if found != -1 {
 			return found + thread*dataXThread
-		} else {
-			return -1
-		}
+		} // else
+		return -1
 	}
 	wg := sync.WaitGroup{}
 	result := make([]int, 0)
@@ -35,11 +35,13 @@ func LinearSearchParallelInt(data []int, target int, thread int) int {
 	return -1
 }
 
+// LinearSearchParallelIntHelper is delegated to search the number and append to the given result array
 func LinearSearchParallelIntHelper(wg *sync.WaitGroup, data []int, target int, result *[]int) {
 	defer wg.Done()
 	*result = append(*result, LinearSearchInt(data, target))
 }
 
+// LinearSearchInt is a simple for delegated to find the target value
 func LinearSearchInt(data []int, target int) int {
 	for i := range data {
 		if target == data[i] {
