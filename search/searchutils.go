06f4a35830c4f8b62732556fd5c44aa26d604ec3
alessiosavi <Alessio.Savi@ibm.com>
2019-10-27 13:18:26 +0100

    First approach to work with goroutins using channel

--- search/searchutils.go
+++ search/searchutils.go
@@ -4,28 +4,36 @@ func LinearSearchParallelInt(data []int, target int, thread int) int {
 	length := len(data)
 	dataXThread := length / thread
 	oddment := length % thread
+
+	result := make(chan int)
 	if oddment != 0 {
 		oddment += thread * dataXThread
-		found := LinearSearchInt(data[thread*dataXThread:oddment], target)
+		go LinearSearchInt(data[thread*dataXThread:oddment], target, result)
+		found := <-result
 		if found != -1 {
 			return found + thread*dataXThread
 		}
 	}
 	for i := 0; i < thread; i++ {
-		index := LinearSearchInt(data[i*dataXThread:(i+1)*dataXThread], target)
+		go LinearSearchInt(data[i*dataXThread:(i+1)*dataXThread], target, result)
+		index := <-result
 		if index != -1 {
 			return index + i*dataXThread
 		}
 	}
-
 	return -1
 }
 
-func LinearSearchInt(data []int, target int) int {
+func LinearSearchInt(data []int, target int, result chan int) {
+	found := false
 	for i := range data {
 		if target == data[i] {
-			return i
+			result <- i
+			found = true
+			return
 		}
 	}
-	return -1
+	if !found {
+		result <- -1
+	}
 }
--- search/searchutils_test.go
+++ search/searchutils_test.go
@@ -10,17 +10,34 @@ const length int = 100000
 
 func TestLinearSearchInt(t *testing.T) {
 	array := helper.GenerateSequentialIntArray(length)
-	t.Log(LinearSearchInt(array, length-1))
+	result := make(chan int)
+	go LinearSearchInt(array, length-1, result)
+	data := <-result
+	t.Log(data)
 }
 
-func TestLinearSearchParallelInt(t *testing.T) {
+func TestOddLinearSearchParallelInt(t *testing.T) {
 	array := helper.GenerateSequentialIntArray(length + 23)
 	t.Log(LinearSearchParallelInt(array, length+22, 10))
 }
+func TestLinearSearchParallelInt(t *testing.T) {
+	array := helper.GenerateSequentialIntArray(length)
+	t.Log(LinearSearchParallelInt(array, length-1, 10))
+}
+
 func BenchmarkLinearSearchInt(t *testing.B) {
+	array := helper.GenerateSequentialIntArray(length)
+	result := make(chan int)
+	t.ResetTimer()
+	for i := 0; i < t.N; i++ {
+		go LinearSearchInt(array, length-1, result)
+		<-result
+	}
+}
+func BenchmarkLinearSearchParallelInt(t *testing.B) {
 	array := helper.GenerateSequentialIntArray(length)
 	t.ResetTimer()
 	for i := 0; i < t.N; i++ {
-		LinearSearchInt(array, length-1)
+		LinearSearchParallelInt(array, length-1, 10)
 	}
 }
