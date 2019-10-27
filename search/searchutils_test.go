baf1c3f9cf844653a5a2ab1a205b99e71ba2af8a
alessiosavi <Alessio.Savi@ibm.com>
2019-10-27 15:46:10 +0100

    Use constant during test

--- search/searchutils_test.go
+++ search/searchutils_test.go
@@ -1,12 +1,14 @@
 package searchutils
 
 import (
+	"math"
 	"testing"
 
 	"github.com/alessiosavi/GoGPUtils/helper"
 )
 
-const length int = 1000000
+const length int = math.MaxInt16
+const thread int = 5
 
 func TestLinearSearchIntOK(t *testing.T) {
 	array := helper.GenerateSequentialIntArray(length)
@@ -25,14 +27,14 @@ func TestLinearSearchIntKO(t *testing.T) {
 
 func TestOddLinearSearchParallelIntOK(t *testing.T) {
 	array := helper.GenerateSequentialIntArray(length + 23)
-	data := LinearSearchParallelInt(array, length+22, 10)
+	data := LinearSearchParallelInt(array, length+22, thread)
 	if data != length+22 {
 		t.Error(data)
 	}
 }
 func TestOddLinearSearchParallelIntKO(t *testing.T) {
 	array := helper.GenerateSequentialIntArray(length + 23)
-	data := LinearSearchParallelInt(array, -1, 10)
+	data := LinearSearchParallelInt(array, -1, thread)
 	if data != -1 {
 		t.Error(data)
 	}
@@ -40,7 +42,7 @@ func TestOddLinearSearchParallelIntKO(t *testing.T) {
 
 func TestLinearSearchParallelIntOK(t *testing.T) {
 	array := helper.GenerateSequentialIntArray(length)
-	data := LinearSearchParallelInt(array, length-1, 10)
+	data := LinearSearchParallelInt(array, length-1, thread)
 	if data != length-1 {
 		t.Error(data)
 	}
@@ -48,7 +50,7 @@ func TestLinearSearchParallelIntOK(t *testing.T) {
 
 func TestLinearSearchParallelIntKO(t *testing.T) {
 	array := helper.GenerateSequentialIntArray(length)
-	data := LinearSearchParallelInt(array, -1, 10)
+	data := LinearSearchParallelInt(array, -1, thread)
 	if data != -1 {
 		t.Error(data)
 	}
@@ -65,6 +67,6 @@ func BenchmarkLinearSearchParallelInt(t *testing.B) {
 	array := helper.GenerateSequentialIntArray(length)
 	t.ResetTimer()
 	for i := 0; i < t.N; i++ {
-		LinearSearchParallelInt(array, length-1, 10)
+		LinearSearchParallelInt(array, length-1, thread)
 	}
 }
