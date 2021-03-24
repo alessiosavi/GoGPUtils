88cdaf26cce14c0691cd1231998d2ffe00224e6c
alessiosavi <alessiosavibtc@gmail.com>
2021-03-24 12:27:56 +0100

    Add method for start a Glue workflow

---
+++ aws/glue/glue.go
@@ -0,0 +1,37 @@
+package glue
+
+import (
+	"context"
+	"errors"
+	stringutils "github.com/alessiosavi/GoGPUtils/string"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/glue"
+)
+
+func StartWorkflow(workflowName string, params map[string]string) error {
+	if stringutils.IsBlank(workflowName) {
+		return errors.New("workflow is empty")
+	}
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return nil
+	}
+
+	glueClient := glue.New(glue.Options{Credentials: cfg.Credentials, Region: cfg.Region})
+	workflow, err := glueClient.GetWorkflow(context.Background(), &glue.GetWorkflowInput{Name: aws.String(workflowName), IncludeGraph: aws.Bool(false)})
+	if err != nil {
+		return err
+	}
+
+	for k, v := range params {
+		workflow.Workflow.DefaultRunProperties[k] = v
+	}
+
+	if _, err = glueClient.UpdateWorkflow(context.Background(), &glue.UpdateWorkflowInput{Name: aws.String(workflowName), DefaultRunProperties: workflow.Workflow.DefaultRunProperties}); err != nil {
+		return err
+	}
+
+	_, err = glueClient.StartWorkflowRun(context.Background(), &glue.StartWorkflowRunInput{Name: aws.String(workflowName)})
+	return err
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.16
 
 require (
 	github.com/alessiosavi/ahocorasick v0.0.3
-	github.com/aws/aws-sdk-go-v2 v1.2.1
+	github.com/aws/aws-sdk-go-v2 v1.3.0
 	github.com/aws/aws-sdk-go-v2/config v1.1.2
 	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.0.3
+	github.com/aws/aws-sdk-go-v2/service/glue v1.2.0
 	github.com/aws/aws-sdk-go-v2/service/lambda v1.1.2
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.2.1
 	github.com/lib/pq v1.10.0
